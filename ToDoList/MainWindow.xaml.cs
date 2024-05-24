0e01c4ac732241e5282f4865c6b46fcaeca6f8eb
Dani0215 <horvathdaniel.lkg@gmail.com>
2024-05-24 15:54:50 +0200

    AddTask method done

--- ToDoList/MainWindow.xaml.cs
+++ ToDoList/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +23,48 @@ namespace ToDoList
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly string filepath = "tasks.json";
+        public ObservableCollection<TaskItem> Tasks { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            Tasks = new ObservableCollection<TaskItem>();
+            LoadTasks();
+            TasksListBox.ItemsSource = Tasks;
+        }
+
+        private void LoadTasks()
+        {
+            if (File.Exists(filepath))
+            {
+                var json = File.ReadAllText(filepath);
+                var tasks = JsonSerializer.Deserialize<ObservableCollection<TaskItem>>(json);
+                if (tasks != null)
+                {
+                    Tasks = tasks;
+                }
+            }
+        }
+
+        private void AddTask_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(TaskTextBox.Text))
+            {
+                Tasks.Add(new TaskItem { Name = TaskTextBox.Text });
+                TaskTextBox.Clear();
+                SaveTask();
+            }
+        }
+
+        private void DeleteTask_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveTask()
+        {
+            throw new NotImplementedException();
         }
     }
 }
